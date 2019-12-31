1efee7fdef273229c9d92f46937dcc910dbdb87f
Brandon <brandon@tendency.me>
2019-12-30 22:45:01 -0600

    feat(fields): Allowing passing default field type values for a more DRY approach

--- src/Field.php
+++ src/Field.php
@@ -2,10 +2,20 @@
 
 namespace Log1x\AcfComposer;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+
 use function Roots\app_path;
 
 abstract class Field
 {
+    /**
+     * Default field type configuration.
+     *
+     * @return array
+     */
+    protected $config = [];
+
     /**
      * Compose the field.
      *
@@ -17,9 +27,35 @@ abstract class Field
             return;
         }
 
+        $this->config = collect($this->config)->mapWithKeys(function ($value, $key) {
+            return [Str::snake($key) => $value];
+        });
+
         add_action('init', function () {
-            acf_add_local_field_group($this->fields());
+            acf_add_local_field_group($this->build());
         }, 20);
+    }
+
+    /**
+     * Build the field group with our field type defaults.
+     *
+     * @return array
+     */
+    protected function build()
+    {
+        return collect($this->fields())->map(function ($value, $key) {
+            if ($key !== 'fields') {
+                return $value;
+            }
+
+            foreach ($value as $field) {
+                if ($this->config->has($field['type'])) {
+                    return [array_merge($field, $this->config->get($field['type']))];
+                }
+            }
+
+            return $value;
+        })->all();
     }
 
     /**
@@ -35,7 +71,7 @@ abstract class Field
             '.' => '/'
         ]);
 
-        return include_once(app_path("Fields/{$name}.php"));
+        return include app_path("Fields/{$name}.php");
     }
 
     /**
