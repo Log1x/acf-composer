70dd276a318cfd34f8525ae66e32a1cdd81e81a0
Philipp <32090713+ouun@users.noreply.github.com>
2025-01-21 11:06:38 +0100

    Update Upgrade Command to remove $block param from `assets()`

--- src/Console/UpgradeCommand.php
+++ src/Console/UpgradeCommand.php
@@ -49,6 +49,7 @@ class UpgradeCommand extends Command
             'public function enqueue($block)' => 'public function assets(array $block): void',
             'public function enqueue($block = [])' => 'public function assets(array $block): void',
             'public function enqueue()' => 'public function assets(array $block): void',
+            'public function enqueue(array $block): void' => 'public function assets(): void',
             '/->addFields\(\$this->get\((.*?)\)\)/' => fn ($match) => "->addPartial({$match[1]})",
             '/->addLayout\(\$this->get\((.*?)\)\)/' => fn ($match) => "->addLayout({$match[1]})",
         ];
