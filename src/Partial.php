088e8fbd6b6d9b072bae55e5751c71f403de955a
Johan van der Molen <hallo@johanvandermolen.nl>
2025-03-13 11:11:33 +0100

    Update Partial.php

--- src/Partial.php
+++ src/Partial.php
@@ -7,11 +7,13 @@ abstract class Partial extends Composer
     /**
      * Compose and register the defined field groups with ACF.
      *
+     * @param array  $args Optional arguments to pass to the partial.
+     *
      * @return \StoutLogic\AcfBuilder\FieldsBuilder|void
      */
-    public function compose()
+    public function compose(array $args = [])
     {
-        $fields = $this->resolveFields();
+        $fields = $this->resolveFields($args);
 
         if (blank($fields)) {
             return;
